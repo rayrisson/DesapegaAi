c74f5ddd6d671d29eac1f9f34ceec1a0b1aef3e8
Rayrisson Vinicius Alves de Lima <rayrissonvinicius@gmail.com>
2023-12-08 10:01:28 -0300

    Refact utilities

--- app/src/main/java/com/example/desapegaai/utilities/EditTextUtilities.kt
+++ app/src/main/java/com/example/desapegaai/utilities/EditTextUtilities.kt
@@ -6,13 +6,13 @@ import android.widget.EditText
 import java.util.Locale
 
 open class EditTextUtilities {
-    fun addDecimalLimiter(editText: EditText, maxLimit: Int = 2) {
+    fun addDecimalLimiter(editText: EditText) {
         editText.addTextChangedListener(object : TextWatcher {
 
             override fun afterTextChanged(s: Editable?) {
                 val str = editText.text!!.toString()
                 if (str.isEmpty()) return
-                val str2 = decimalLimiter(str, maxLimit)
+                val str2 = formatString(str)
 
                 if (str2 != str) {
                     editText.setText(str2)
@@ -32,41 +32,8 @@ open class EditTextUtilities {
         })
     }
 
-    private fun decimalLimiter(string: String, MAX_DECIMAL: Int): String {
+    private fun formatString(string: String): String {
         val str = string.replace(".", "")
         return "%,.0f".format(Locale.GERMAN, str.toDouble())
-
-
-//        var str = string
-//        if (str[0] == '.') str = "0$str"
-//        val max = str.length
-//
-//        var rFinal = ""
-//        var after = false
-//        var i = 0
-//        var up = 0
-//        var decimal = 0
-//        var t: Char
-//
-//        val decimalCount = str.count{ ".".contains(it) }
-//
-//        if (decimalCount > 1)
-//            return str.dropLast(1)
-//
-//        while (i < max) {
-//            t = str[i]
-//            if (t != '.' && !after) {
-//                up++
-//            } else if (t == '.') {
-//                after = true
-//            } else {
-//                decimal++
-//                if (decimal > MAX_DECIMAL)
-//                    return rFinal
-//            }
-//            rFinal += t
-//            i++
-//        }
-//        return rFinal
     }
 }
